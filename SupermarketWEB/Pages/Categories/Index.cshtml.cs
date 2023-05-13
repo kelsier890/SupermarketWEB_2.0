ed4511b6b750bcf0b3b2d815fa75880e3306e125
kelsier890 <jeromonto890@gmail.com>
2023-05-13 11:12:42 -0500

    Index Categories

---
+++ SupermarketWEB/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+        public IndexModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+        public IList<Category> Categories { get; set; } = default!;
+
+        public async Task OneGetAsync()
+        {
+            if (_context.Categories != null)
+            {
+                Categories=await _context.Categories.ToListAsync();
+            }
+        }
+    }
+}
