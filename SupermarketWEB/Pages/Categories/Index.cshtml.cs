6ba495e46aa8e92f4df13556971bd413ab262d95
kelsier890 <jeromonto890@gmail.com>
2023-05-19 14:40:37 -0500

    student

--- SupermarketWEB/Pages/Categories/Index.cshtml.cs
+++ SupermarketWEB/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -6,8 +7,20 @@ using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Categories
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
         private readonly SumpermarketContext _context;
         public IndexModel(SumpermarketContext context)
         {
